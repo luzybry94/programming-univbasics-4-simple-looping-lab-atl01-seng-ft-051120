3386e62baea41aa5bb5da3d92717e79b99b0e314
Chaim Bryski <luzybry94@gmail.com>
2020-04-22 02:02:16 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,10 @@
 # Write your methods here
 def loop_message_five_times(string)
   count = 0
-  while count < 6 do
+  while count < 5 do
     puts string
-    count 
+    count += 1
+  end
 end
 
 def loop_message_n_times(string, num)
